81505bd78a3c1fd1e492b356e4633c6475d402b0
Nisha <nishan.inditronics@gmail.om>
2025-12-03 17:08:38 +0530

    ec2

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '127.0.0.1', () => {
+  console.log(`Backend running on http://127.0.0.1:${PORT}`);
 });
